e62bf870b72f9b3154a8981963efd610ea805f63
MysticalStorm <MysticalStorming@gmail.com>
2017-04-29 10:26:00 +0300

    Update injection script. Update game script matching

--- network_injector.js
+++ network_injector.js
@@ -1,6 +1,8 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
-    if (info.url === "https://data.mo.ee/release.js?v=57-7-0-2") {
+    var regexp = /https:\/\/data.mo.ee\/release.js\?v=(\d{2}|\d{1})-(\d{2}|\d{1})-(\d{2}|\d{1})-(\d{2}|\d{1})(\w)?/g
+    if (regexp.test( info.url.toString() )) {
+    //if (info.url === "https://data.mo.ee/release.js?v=57-7-0-2a") {
       return {cancel: true};
     }
   },
